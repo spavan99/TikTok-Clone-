ecb805a5fe6e6638c763fa415fbec6990bfb8a0c
spavan99 <spavan@terra.com.br>
2023-03-30 10:25:02 +0100

    start TikTok Clone

---
+++ git-start.bat
@@ -0,0 +1,7 @@
+echo "# TikTok-Clone-" >> README.md
+git init
+git add .
+git commit -m "start TikTok Clone"
+git branch -M main
+git remote add origin https://github.com/spavan99/TikTok-Clone-.git
+git push -u origin main
---
+++ src/pages/Video.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from 'react';
+import './video.css';
+
+function Video() {
+  const videoRef = useRef(null);
+
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.play();
+      setPlay(false);
+    } else {
+      videoRef.current.pause();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
