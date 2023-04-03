c784f6bf106ef6a71d158057b3ff0f8f2f4a0de9
spavan99 <spavan@terra.com.br>
2023-04-03 18:12:43 +0100

    sexta aula - finalizaçao grafica e publicacao

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 import Video from './pages/Video';
 import db from './config/firebase';
 import { collection, getDocs } from 'firebase/firestore/lite';
+import { display } from '@mui/system';
+import { NoEncryption } from '@mui/icons-material';
 
 function App() {
+  let maximoHeight;
+  if (window.innerHeight <= 800) {
+    maximoHeight = window.innerHeight;
+  }
+
   const [video, setVideos] = useState([]);
 
   async function getVideos() {
@@ -19,7 +26,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maximoHeight + 'px' }}>
       <div className="app__videos">
         {video.map(item => {
           return (
@@ -43,16 +50,6 @@ function App() {
           description="Brecker o goleiro"
           music="musica epica"
           url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
-        /> */}
-
-        {/* <Video
-          likes={444}
-          c={555}
-          shares={666}
-          name="spavan"
-          description="Bird olhando para a camara"
-          music="Clap your hands"
-          url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/bird.mp4?alt=media&token=52abbeec-ff95-4acb-808e-5a4b4977d1da"
         /> */}
       </div>
     </div>
